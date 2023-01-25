86aa050bb6e5da83f120e790f55789b2323c1488
LinkHere <99461450+LinkHere@users.noreply.github.com>
2023-01-25 09:45:01 +0800

    Create deleteemptyfolders.js

---
+++ deleteemptyfolders.js
@@ -0,0 +1,27 @@
+function deleteEmptyFolders() {
+  const PARENT_FOLDER_ID = "1y-wg3aTHMtMnYQL45uyWDq_w_gOOUiZj";
+  const parentFolder = DriveApp.getFolderById(PARENT_FOLDER_ID);
+  const folders = parentFolder.getFolders();
+  while (folders.hasNext()) {
+    const subFolder = folders.next();
+    treeFolder(parentFolder, subFolder);
+  }
+}
+
+function treeFolder(parentFolder, folder) {
+
+
+  const childfolders = folder.getFolders();      
+
+  while (childfolders.hasNext()) {               
+    const childfolder = childfolders.next();
+    treeFolder(folder, childfolder);
+  }
+
+  const hasFile = folder.getFiles().hasNext();               
+  const hasFolder = folder.getFolders().hasNext();               
+
+  if (!hasFile && !hasFolder) {            
+    folder.setTrashed(true)
+  }             
+}
